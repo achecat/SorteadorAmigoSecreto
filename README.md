4bab238c069b0a3ed50ca88aa9c036743c6877df
Gabriel Muza <achecat@outlook.com>
2025-01-31 11:23:34 -0300

    Conclusao do js e do readme

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# SorteadorAmigoSecreto
+# Amigo Secreto - Sorteio Interativo
+
+O Amigo Secreto é uma aplicação web simples e divertida para organizar sorteios de amigo secreto entre amigos ou familiares. Através dessa aplicação, você pode facilmente adicionar os nomes dos participantes e, com apenas um clique, realizar o sorteio de maneira aleatória e justa!
+
+
+## Funcionalidades:
+
+* Adicionar participantes: Insira os nomes dos seus amigos na lista.
+
+* Sorteio de amigo secreto: Clique em "Sortear" para que a aplicação escolha um participante aleatoriamente.
+
+* Reiniciar o sorteio: Após o sorteio, você pode reiniciar o processo e adicionar novos participantes ou repetir o sorteio.
+
+*Validação de entrada: A aplicação verifica se há pelo menos dois participantes antes de realizar o sorteio, garantindo que o processo seja válido.
+
+
+## Como funciona:
+
+* 1 - Adicionar nomes: Preencha o campo de texto com o nome dos participantes e clique em "Adicionar" para incluir na lista.
+
+* 2 - Sorteio: Quando estiver pronto para sortear, clique no botão "Sortear". A aplicação escolherá aleatoriamente um nome da lista e exibirá a mensagem de quem foi sorteado.
+
+* 3 - Reiniciar: Após o sorteio, você pode clicar no botão "Reiniciar" para limpar a lista e começar o processo novamente, se necessário.
+
+
+## Tecnologias utilizadas:
+
+* HTML para estruturar a página.
+* CSS para estilização e design da interface.
+* JavaScript para a lógica de adicionar nomes, sortear e reiniciar o processo
--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 /*Criacao da lista com os nomes para sortear*/
 let listaDeNomes = [];
+
 /*Pega uma tag e exibe um texto especifico naquela tag*/
 function exibirTextoNaTela(tag, texto){
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
+
 /*Adiciona o valor do input na lista com os nomes*/
 function adicionarNoArray(){
     let inputNome = document.getElementById("idDoInput");
@@ -16,9 +18,10 @@ function adicionarNoArray(){
         inputNome.value = "";
     }
     else{
-        alert('Digite algo antes de adicionar')
+        alert('Digite algo antes de adicionar!');
     }
 }
+
 /*Pega o conteudo da lista com os nomes e coloca cada um em uma li no index*/
 function atualizarLista(){
     let lista = document.getElementById('listaDeAmigos');
@@ -27,11 +30,32 @@ function atualizarLista(){
     listaDeNomes.forEach((nome) =>{
         let li = document.createElement("li");
         li.textContent = nome;
-        lista.appendChild(li)
+        lista.appendChild(li);
     });
 }
+
 /*Faz o sorteio do nome*/
 function sortear(){
+    if (listaDeNomes <= 1){
+        alert("Adicione nomes antes de sortear!");
+        return
+    }
+
     let sorteado = listaDeNomes[Math.floor(Math.random() * listaDeNomes.length)];
-    exibirTextoNaTela("h2", `Parabens ${sorteado}, você foi escolhido`)
+    exibirTextoNaTela("h2", `Parabéns ${sorteado}, você foi escolhido!`);
+
+    document.getElementById("buttonSortear").style.display = "none";
+    document.getElementById("buttonReiniciar").style.display = "inline-block";
+}
+
+/*Reinicia toda a aplicacao, limpando a lista e liberando o input*/
+function reiniciar(){
+    listaDeNomes = [];
+
+    exibirTextoNaTela("h2", "Digite o nome dos seus amigos!")
+
+    atualizarLista()
+
+    document.getElementById("buttonReiniciar").style.display = "none";
+    document.getElementById("buttonSortear").style.display = "inline-block";
 }
